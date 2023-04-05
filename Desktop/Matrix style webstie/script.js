3e0ced4c07c1ebd756c3df0af99f3aedeb8b54f4
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-04-06 01:32:41 +0200

    Added mouse following

--- Desktop/Matrix style webstie/script.js
+++ Desktop/Matrix style webstie/script.js
@@ -1,37 +1,46 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const fontSize = 16;
-const columns = Math.floor(window.innerWidth / fontSize);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const wordList = ['love', 'dumplings', 'heart', 'affection', 'devotion', 'admiration', 'adoration', 'passion', 'fondness', 'warmth', 'tenderness', 'caring', 'sweetheart', 'beloved', 'cherished', 'darling', 'dear', 'valentine', 'hug', 'kiss'];
 
-let charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-let customWords = [];
+let x = 0;
+let y = 0;
 
-
-
-let matrix = [];
-
-for (let i = 0; i < columns; i++) {
-  matrix.push(Math.floor(Math.random() * canvas.height / 2) * -1);
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
 function draw() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
   ctx.fillStyle = '#0F0';
-  ctx.font = `${fontSize}px monospace`;
-
-  for (let i = 0; i < matrix.length; i++) {
-    const char = charSet[Math.floor(Math.random() * charSet.length)];
-    ctx.fillText(char, i * fontSize, matrix[i] * fontSize);
-    matrix[i]++;
-    if (matrix[i] * fontSize > canvas.height && Math.random() > 0.975) {
-      matrix[i] = 0;
-    }
+  ctx.font = '20px Arial';
+  for(let i = 0; i < wordList.length; i++) {
+    const word = wordList[i];
+    const xPos = x + i * 20;
+    const yPos = y + i * 20;
+    ctx.fillText(word, xPos, yPos);
+  }
+  x -= 1;
+  y += 1;
+  if(x < -canvas.width) {
+    x = 0;
+  }
+  if(y > canvas.height) {
+    y = 0;
   }
 }
 
-setInterval(draw, 50);
+window.addEventListener('resize', resizeCanvas);
+window.addEventListener('load', function() {
+  resizeCanvas();
+  setInterval(draw, 50);
+});
+
+function addWord() {
+  const newWord = prompt('Enter a new word:');
+  if(newWord) {
+    wordList.push(newWord);
+  }
+}
